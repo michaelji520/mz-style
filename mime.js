874fc9f71fecf526acdebfbc24b52aad03011137
zhangji01 <zhangji01@zuoyebang.com>
2018-02-11 11:58:05 +0800

    init style

---
+++ mime.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2014-2018 Zuoyebang, All rights reserved.
+ * @fileoverview
+ * @author zhangji01 | zhangji01@zuoyebang.com
+ * @version 1.0 | 2018-02-06 | initial version
+ */
+
+exports.types = {
+  "css": "text/css",
+  "gif": "image/gif",
+  "html": "text/html",
+  "ico": "image/x-icon",
+  "jpeg": "image/jpeg",
+  "jpg": "image/jpeg",
+  "js": "text/javascript",
+  "json": "application/json",
+  "pdf": "application/pdf",
+  "png": "image/png",
+  "svg": "image/svg+xml",
+  "swf": "application/x-shockwave-flash",
+  "tiff": "image/tiff",
+  "txt": "text/plain",
+  "wav": "audio/x-wav",
+  "wma": "audio/x-ms-wma",
+  "wmv": "video/x-ms-wmv",
+  "xml": "text/xml"
+};
