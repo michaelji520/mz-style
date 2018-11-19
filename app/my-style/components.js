26e6cae0eb15d19003e54a1032025d6c2daf9e6f
zhangji02 <zhangji02@zuoyebang.com>
2018-11-19 20:33:14 +0800

    [UPDATE] add icons

---
+++ app/my-style/components.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2015-2018 Nighthawk Studio, All rights reserved.
+ * @fileoverview Frondend js components
+ * @author Michael Zhang | michaelji520@gmail.com
+ * @version 1.0 | 2018-11-19
+ */
+
+let message = function (params) {
+  let {
+    type = 'info',
+    content = '',
+    duration = 3000
+  } = params;
+
+};
